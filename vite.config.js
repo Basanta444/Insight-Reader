cecb4d999f3e9c5aea66dba10b2cb43d050c37be
Basanta Khadka <basantakhadka84@gmail.com>
2026-06-28 21:47:00 +0545

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', // ✅ important for Vercel deployments
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['.replit.dev'],
-  },
 });
